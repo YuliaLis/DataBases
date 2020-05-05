8ec94b2ab271a9b7c9e6b729fc088051a5b067ea
YuliaLis <44850179+YuliaLis@users.noreply.github.com>
2020-05-05 21:51:20 +0300

    Update kaggle_import.py

--- Laboratory 3/kaggle_import.py
+++ Laboratory 3/kaggle_import.py
@@ -22,40 +22,28 @@ filename='serialsinfo.csv'
 file = open(filename, errors='ignore')
 rows = csv.reader(file, delimiter=',')
 
-genre = []
-rating = []
+genres = []
 
+next(rows)
 for data in rows:
     serial_name = data[12].strip()
     genre_kind = data[5].strip()
-    rating_value = data[17].strip()
 
     genre_kind = re_split(r',', data[5])
 
     cursor.execute('INSERT INTO serials(serial_name) VALUES (:serial_name)', serial_name=serial_name)
     connection.commit()
 
-    for genr in genre_kind:
-        if genr not in genre:
-            genre.append(genr)
-            cursor.execute('INSERT INTO genres(genre_kind) VALUES (:genre_kind)', genre_kind=genr)
+    for genre in genre_kind:
+        if genre not in genres:
+            genres.append(genre)
+            cursor.execute('INSERT INTO genres(genre_kind) VALUES (:genre_kind)', genre_kind=genre)
             connection.commit()
 
-    cursor.execute('INSERT INTO serialsgenres(serial_name,genre_kind) VALUES (:serial_name, :genre_kind)', serial_name=serial_name, genre_kind=genr)
-    connection.commit()
-
-    for rate in rating_value:
-        if rate not in rating:
-            rating.append(rate)
-            cursor.execute('INSERT INTO ratings(rating_value) VALUES (:rating_value)', rating_value=rate)
-
-    cursor.execute('INSERT INTO serialsratings(serial_name,rating_value) VALUES (:serial_name, :rating_value)',
-                   serial_name=serial_name, rating_value=rate)
-    connection.commit()
-
+    for genre in genre_kind:
+        cursor.execute('INSERT INTO serialsgenres(serial_name,genre_kind) VALUES (:serial_name, :genre_kind)', serial_name=serial_name, genre_kind=genre)
+        connection.commit()
+ 
 cursor.close()
 connection.close()
 file.close()
-
-
-
